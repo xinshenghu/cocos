377c061a322434f5df030c033544b315627c34cd
xinshenghu <235389963@qq.com>
2017-05-27 07:17:00 +0800

    1

---
+++ dadishu/main.js
@@ -0,0 +1,16 @@
+cc.game.onStart = function () {
+    //cc.view.adjustViewPort(true);
+    if(!cc.sys.isNative && document.getElementById("cocosLoading")) //If referenced loading.js, please remove it
+        document.body.removeChild(document.getElementById("cocosLoading"));
+
+    cc.view.setDesignResolutionSize(640, 900, cc.ResolutionPolicy.SHOW_ALL);
+    cc.view.resizeWithBrowserSize(true);
+
+    cc.LoaderScene.preload(g_resources, function () {
+
+        cc.director.runScene(new HelloWorldScene());
+
+    }, this);
+
+};
+cc.game.run();
